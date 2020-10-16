27498b5ec3d63403343dea8a9d50e81a4dd9a8b7
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-16 22:07:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,7 +124,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages].insert "Assembly"
+programmer_hash[:dennis_ritchie][:languages].push "Assembly"
 
 return programmer_hash
 
